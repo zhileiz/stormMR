8f20512617e782101de4f3a6846a8518cada7128
NETS user <zivesnets@gmail.com>
2015-10-22 14:47:25 -0400

    Initial

---
+++ src/edu/upenn/cis455/mapreduce/job/WordCount.java
@@ -0,0 +1,22 @@
+package edu.upenn.cis455.mapreduce.job;
+
+import edu.upenn.cis455.mapreduce.Context;
+import edu.upenn.cis455.mapreduce.Job;
+
+public class WordCount implements Job {
+
+  public void map(String key, String value, Context context)
+  {
+    // Your map function for WordCount goes here
+
+
+  }
+  
+  public void reduce(String key, String[] values, Context context)
+  {
+    // Your reduce function for WordCount goes here
+
+
+  }
+  
+}
